5b232a083a49b67bd82fe771ff0d279d43a28f8b
isabelyseguettofprofissional-eng <isabelyseguettof.profissional@gmail.com>
2026-03-09 22:50:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-function subscribe(){
+const images1 = [
+"https://picsum.photos/600/400?1",
+"https://picsum.photos/600/400?3",
+"https://picsum.photos/600/400?5"
+];
 
-let message = document.getElementById("message");
+const images2 = [
+"https://picsum.photos/600/400?2",
+"https://picsum.photos/600/400?4",
+"https://picsum.photos/600/400?6"
+];
 
-message.innerText = "Thank you for subscribing to Global Teen Dispatch!";
+let i = 0;
 
-}
+setInterval(()=>{
+
+i = (i+1) % images1.length;
+
+document.getElementById("img1").style.opacity=0;
+document.getElementById("img2").style.opacity=0;
+
+setTimeout(()=>{
+
+document.getElementById("img1").src = images1[i];
+document.getElementById("img2").src = images2[i];
+
+document.getElementById("img1").style.opacity=1;
+document.getElementById("img2").style.opacity=1;
+
+},500)
+
+},4000)
